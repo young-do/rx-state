6c60284480f6606e0b61b68499b57e59d81ed11d
racoon.cho <racoon.cho@kakaoenterprise.com>
2021-12-14 16:58:21 +0900

    hook 수정

--- packages/rxjs-store-react/src/hooks/useRxState.ts
+++ packages/rxjs-store-react/src/hooks/useRxState.ts
@@ -2,7 +2,7 @@ import { RxState } from 'rxjs-store-core';
 import { useEffect, useState } from 'react';
 
 export const useRxState = <T>(rxState: RxState<T>) => {
-  const [state, setState] = useState<T>();
+  const [state, setState] = useState<T>(rxState.value);
   const set = rxState.set;
 
   useEffect(() => {
@@ -14,7 +14,7 @@ export const useRxState = <T>(rxState: RxState<T>) => {
 };
 
 export const useRxValue = <T>(rxState: RxState<T>) => {
-  const [state, setState] = useState<T>();
+  const [state, setState] = useState<T>(rxState.value);
 
   useEffect(() => {
     const subscription = rxState.subscribe(value => setState(value));

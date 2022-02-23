191821163aa8efe1f422d542f786d6ff662c502b
young-do <yd199323@gmail.com>
2022-02-23 10:29:15 +0900

    remove private field

--- src/state.ts
+++ src/state.ts
@@ -2,19 +2,19 @@ import { BehaviorSubject } from 'rxjs';
 import cloneDeep from 'lodash.clonedeep';
 
 export class State<T, R = T> extends BehaviorSubject<T> {
-  #rootState?: State<R>;
-  #path?: string;
-  #memo = Object.create(null);
+  private _rootState?: State<R>;
+  private _path?: string;
+  private _memo = Object.create(null);
 
-  constructor(value: T, path?: string, rootState?: State<R>) {
+  constructor(value: T, _path?: string, _rootState?: State<R>) {
     super(value);
-    this.#path = path;
-    this.#rootState = rootState;
+    this._path = _path;
+    this._rootState = _rootState;
 
-    if (path && rootState) {
+    if (_path && _rootState) {
       // syncing
-      rootState.subscribe(rootValue => {
-        const nestedValue = getValue<T>(rootValue, path);
+      _rootState.subscribe(rootValue => {
+        const nestedValue = getValue<T>(rootValue, _path);
         // @note: set을 호출하면 loop에 빠지므로 next 호출.
         this.next(nestedValue as T);
       });
@@ -22,51 +22,51 @@ export class State<T, R = T> extends BehaviorSubject<T> {
   }
 
   set(nextValue: T): void {
-    if (this.#rootState && this.#path) {
+    if (this._rootState && this._path) {
       // upcast
-      const merged = mergeValue(this.#rootState.value, this.#path, nextValue);
-      this.#rootState.set(merged);
+      const merged = mergeValue(this._rootState.value, this._path, nextValue);
+      this._rootState.set(merged);
     } else {
       this.next(nextValue);
     }
   }
 
-  partial<P>(path: string): State<P> {
-    if (this.#memo[path]) return this.#memo[path];
+  partial<P>(_path: string): State<P> {
+    if (this._memo[_path]) return this._memo[_path];
 
-    const partialValue = getValue<P>(this.value, path) as P;
-    const partialState = new State<P, T>(partialValue, path, this);
+    const partialValue = getValue<P>(this.value, _path) as P;
+    const partialState = new State<P, T>(partialValue, _path, this);
 
-    return (this.#memo[path] = partialState);
+    return (this._memo[_path] = partialState);
   }
 }
 
-export function getValue<T>(object: any, path: string): T | undefined {
-  const paths = path.split('/').filter(Boolean);
+export function getValue<T>(object: any, _path: string): T | undefined {
+  const _paths = _path.split('/').filter(Boolean);
   let nested = object;
 
-  paths.every(key => (nested = nested[key]));
+  _paths.every(key => (nested = nested[key]));
   return nested;
 }
 
-export function mergeValue<T>(object: any, path: string, value: T): any {
-  const paths = path.split('/').filter(Boolean);
-  const lastIndex = paths.length - 1;
+export function mergeValue<T>(object: any, _path: string, value: T): any {
+  const _paths = _path.split('/').filter(Boolean);
+  const lastIndex = _paths.length - 1;
 
-  if (paths.length === 0) {
+  if (_paths.length === 0) {
     return typeof value === 'object' ? { ...value } : value;
   }
 
   const cloned = cloneDeep(object);
   let nested = cloned;
-  paths.forEach((path, index) => {
-    if (typeof nested[path] !== 'object') {
-      nested[path] = Object.create(null);
+  _paths.forEach((_path, index) => {
+    if (typeof nested[_path] !== 'object') {
+      nested[_path] = Object.create(null);
     }
     if (index === lastIndex) {
-      nested[path] = typeof value === 'object' ? { ...nested[path], ...value } : value;
+      nested[_path] = typeof value === 'object' ? { ...nested[_path], ...value } : value;
     }
-    nested = nested[path];
+    nested = nested[_path];
   });
   return cloned;
 }

7b4325155aaf1edc18c66efb613ada2a3c85b2fa
young-do <yd199323@gmail.com>
2022-07-10 16:35:23 +0900

    use vitest instead of jest

--- test/state.test.ts
+++ test/state.test.ts
@@ -1,88 +1,7 @@
+import { describe, it, expect, vi } from 'vitest';
 import { atom } from '../src';
-import { mergeValue } from '../src/state';
 
 describe('state test', () => {
-  describe('merge function test', () => {
-    it('same depth - case 1', () => {
-      const origin = { a: 1 };
-      const next = mergeValue(origin, '/a', 2);
-      expect(origin).toEqual({ a: 1 });
-      expect(next).toEqual({ a: 2 });
-    });
-    it('same depth - case 2', () => {
-      const origin = { a: 1 };
-      const next = mergeValue(origin, '/b', 1);
-      expect(origin).toEqual({ a: 1 });
-      expect(next).toEqual({ a: 1, b: 1 });
-    });
-    it('deeper', () => {
-      const origin = { a: 1 };
-      const next = mergeValue(origin, '/a/b/c', 1);
-      expect(origin).toEqual({ a: 1 });
-      expect(next).toEqual({ a: { b: { c: 1 } } });
-    });
-    it('swallower - case 1', () => {
-      const origin = { a: { a: 1 } };
-      const next = mergeValue(origin, '/a', 1);
-      expect(origin).toEqual({ a: { a: 1 } });
-      expect(next).toEqual({ a: 1 });
-    });
-    it('swallower - case 2', () => {
-      const origin = { a: { a: 1 } };
-      const next = mergeValue(origin, '/b', 1);
-      expect(origin).toEqual({ a: { a: 1 } });
-      expect(next).toEqual({ a: { a: 1 }, b: 1 });
-    });
-    it('set on root', () => {
-      const origin = { a: 1 };
-      const next = mergeValue(origin, '/', { b: 1 });
-      expect(origin).toEqual({ a: 1 });
-      expect(next).toEqual({ b: 1 });
-    });
-    it('[error case] when primitive type', () => {
-      const origin = 'hello world';
-      expect(() => mergeValue(origin, '/a', 2)).toThrowError();
-    });
-  });
-
-  describe('partial test', () => {
-    it('when primitive type', () => {
-      const state = atom(0);
-      const p1 = state.partial('/');
-      const p2 = state.partial('/hello/world');
-
-      expect(state.value).toBe(0);
-      expect(p1.value).toBe(state.value);
-      expect(p2.value).toBeUndefined();
-    });
-
-    it('when object', () => {
-      const state = atom<object>({ hello: { world: 'it works!' } });
-      const p1 = state.partial('/');
-      const p2 = state.partial('/hello/world');
-      const p3 = state.partial('/hello/wow');
-
-      expect(state.value).toEqual({ hello: { world: 'it works!' } });
-      expect(p1.value).toEqual(state.value);
-      expect(p2.value).toBe('it works!');
-      expect(p3.value).toBeUndefined();
-
-      state.set({ hello: { wow: 'it works!' } });
-
-      expect(state.value).toEqual({ hello: { wow: 'it works!' } });
-      expect(p1.value).toEqual(state.value);
-      expect(p2.value).toBeUndefined();
-      expect(p3.value).toBe('it works!');
-
-      p3.set('amazing!');
-
-      expect(state.value).toEqual({ hello: { wow: 'amazing!' } });
-      expect(p1.value).toEqual(state.value);
-      expect(p2.value).toBeUndefined();
-      expect(p3.value).toBe('amazing!');
-    });
-  });
-
   describe('update test', () => {
     it('when primitive type, update fn works', () => {
       const state = atom(0);
@@ -102,33 +21,34 @@ describe('state test', () => {
   });
 
   describe('callback test', () => {
-    it('allow that callback is async', done => {
-      let called = false;
-      const state = atom('hello', undefined, async state => {
-        called = true;
-        state.set(state.value + ' world');
-      });
-      state.subscribe(value => {
-        if (!called) return;
+    it('allow that callback is async', () =>
+      new Promise<void>(done => {
+        let called = false;
+        const state = atom('hello', undefined, state => {
+          called = true;
+          state.set(`${state.value} world`);
+        });
+        state.subscribe(value => {
+          if (!called) return;
 
-        expect(called).toBe(true);
-        expect(value).toBe('hello world');
-        done();
-      });
-    });
+          expect(called).toBe(true);
+          expect(value).toBe('hello world');
+          done();
+        });
+      }));
   });
 
   describe('basic test', () => {
     it('when no default value, subscribe fn is not called', () => {
       const state = atom<number>();
-      const mockFn = jest.fn();
+      const mockFn = vi.fn();
       state.subscribe(mockFn);
       expect(mockFn).not.toHaveBeenCalled();
     });
 
     it('when default value, subscribe fn is called', () => {
       const state = atom(1);
-      const mockFn = jest.fn();
+      const mockFn = vi.fn();
       state.subscribe(mockFn);
       expect(mockFn).toHaveBeenCalled();
     });

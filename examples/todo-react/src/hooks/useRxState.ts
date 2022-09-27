d2c2d8449b45ee74513f6192477faff82b670546
young-do <yd199323@gmail.com>
2022-09-27 10:51:55 +0900

    use useSyncExternalStore

--- examples/todo-react/src/hooks/useRxState.ts
+++ examples/todo-react/src/hooks/useRxState.ts
@@ -1,25 +1,43 @@
 import { RxState } from '@youngdo/rx-state';
-import { useEffect, useState } from 'react';
+import { useSyncExternalStore } from 'use-sync-external-store/shim';
 
 export const useRxState = <T>(rxState: RxState<T>) => {
-  const [state, setState] = useState<T>(rxState.value);
+  const subscribe = (callback: () => void) => () => rxState.subscribe(callback);
+  const getSnapshot = () => rxState.value;
+  const state = useSyncExternalStore(subscribe, getSnapshot, getSnapshot);
   const set = rxState.set;
 
-  useEffect(() => {
-    const subscription = rxState.subscribe(value => setState(value));
-    return () => subscription.unsubscribe();
-  }, []);
-
-  return [state, set];
+  return [state, set] as const;
 };
 
 export const useRxValue = <T>(rxState: RxState<T>) => {
-  const [state, setState] = useState<T>(rxState.value);
-
-  useEffect(() => {
-    const subscription = rxState.subscribe(value => setState(value));
-    return () => subscription.unsubscribe();
-  }, []);
+  const subscribe = (callback: () => void) => () => rxState.subscribe(callback);
+  const getSnapshot = () => rxState.value;
+  const state = useSyncExternalStore(subscribe, getSnapshot, getSnapshot);
 
   return state;
 };
+
+// @note: if you cannot use "useSyncExternalStore" hook, use below code.
+// export const useRxState = <T>(rxState: RxState<T>) => {
+//   const [state, setState] = useState<T>(rxState.value);
+//   const set = rxState.set;
+
+//   useEffect(() => {
+//     const subscription = rxState.subscribe(value => setState(value));
+//     return () => subscription.unsubscribe();
+//   }, []);
+
+//   return [state, set];
+// };
+
+// export const useRxValue = <T>(rxState: RxState<T>) => {
+//   const [state, setState] = useState<T>(rxState.value);
+
+//   useEffect(() => {
+//     const subscription = rxState.subscribe(value => setState(value));
+//     return () => subscription.unsubscribe();
+//   }, []);
+
+//   return state;
+// };

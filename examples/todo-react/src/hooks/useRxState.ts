9e99be16367603d90cbbfa7c6a9918a724cbd83b
young-do <yd199323@gmail.com>
2022-10-13 14:35:18 +0900

    remove use-sync-external-store

--- examples/todo-react/src/hooks/useRxState.ts
+++ examples/todo-react/src/hooks/useRxState.ts
@@ -1,43 +1,25 @@
 import { RxState } from '@youngdo/rx-state';
-import { useSyncExternalStore } from 'use-sync-external-store/shim';
+import { useState, useEffect } from 'react';
 
 export const useRxState = <T>(rxState: RxState<T>) => {
-  const subscribe = (callback: () => void) => () => rxState.subscribe(callback);
-  const getSnapshot = () => rxState.value;
-  const state = useSyncExternalStore(subscribe, getSnapshot, getSnapshot);
+  const [state, setState] = useState<T>(rxState.value);
   const set = rxState.set;
 
-  return [state, set] as const;
+  useEffect(() => {
+    const subscription = rxState.subscribe(value => setState(value));
+    return () => subscription.unsubscribe();
+  }, []);
+
+  return [state, set];
 };
 
 export const useRxValue = <T>(rxState: RxState<T>) => {
-  const subscribe = (callback: () => void) => () => rxState.subscribe(callback);
-  const getSnapshot = () => rxState.value;
-  const state = useSyncExternalStore(subscribe, getSnapshot, getSnapshot);
+  const [state, setState] = useState<T>(rxState.value);
+
+  useEffect(() => {
+    const subscription = rxState.subscribe(value => setState(value));
+    return () => subscription.unsubscribe();
+  }, []);
 
   return state;
 };
-
-// @note: if you cannot use "useSyncExternalStore" hook, use below code.
-// export const useRxState = <T>(rxState: RxState<T>) => {
-//   const [state, setState] = useState<T>(rxState.value);
-//   const set = rxState.set;
-
-//   useEffect(() => {
-//     const subscription = rxState.subscribe(value => setState(value));
-//     return () => subscription.unsubscribe();
-//   }, []);
-
-//   return [state, set];
-// };
-
-// export const useRxValue = <T>(rxState: RxState<T>) => {
-//   const [state, setState] = useState<T>(rxState.value);
-
-//   useEffect(() => {
-//     const subscription = rxState.subscribe(value => setState(value));
-//     return () => subscription.unsubscribe();
-//   }, []);
-
-//   return state;
-// };
